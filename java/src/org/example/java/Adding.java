04c2c8240356a767b737fb110a1605b8f032dde4
Ravula vamsikrishna <vamshikrishna.r@techouts.com>
2022-03-16 17:33:08 +0530

    first java example

---
+++ java/src/org/example/java/Adding.java
@@ -0,0 +1,10 @@
+package org.example.java;
+import org.test.java.*;
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Add.a);
+	}
+
+}
