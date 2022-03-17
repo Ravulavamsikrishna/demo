e676f12aa18817830fa3ea7c1f197535eb92cd31
Ravula vamsikrishna <vamshikrishna.r@techouts.com>
2022-03-17 13:27:52 +0530

    first commit

---
+++ java/src/org/test/java/Add.java
@@ -0,0 +1,6 @@
+package org.test.java;
+
+public class Add {
+	public static int a=10;
+	static int b=20;
+}
---
+++ java/src/org/test/java/Test.java
@@ -0,0 +1,10 @@
+package org.test.java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println(Add.a+Add.b);
+	}
+
+}
